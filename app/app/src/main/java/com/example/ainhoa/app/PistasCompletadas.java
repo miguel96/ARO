0396343f1a7fc3bb1a57ebb0d28a3d030fdf90e7
ruben <rufopufo@hotmail.com>
2018-03-07 15:50:47 +0100

    clases Usuario, ProgresoHistoria, PistasCompletadas

---
+++ app/app/src/main/java/com/example/ainhoa/app/PistasCompletadas.java
@@ -0,0 +1,24 @@
+package com.example.ainhoa.app;
+
+import java.util.HashMap;
+
+/**
+ * Created by ainhoa on 31/01/18.
+ */
+
+public class PistasCompletadas {
+
+    private String idPista;
+    private Date fechaCompletada;
+
+
+
+    public String getIdPista(){
+        return this.idPista;
+    }
+
+    public Date getFechaCompletada{
+        return this.fechaCompletada;
+    }
+
+}
---
+++ app/app/src/main/java/com/example/ainhoa/app/ProgresoHistoria.java
@@ -0,0 +1,28 @@
+package com.example.ainhoa.app;
+
+import java.util.HashMap;
+
+/**
+ * Created by ainhoa on 31/01/18.
+ */
+
+public class ProgresoHistoria {
+
+    private String idHistoria;
+    private ArrayList<PistasCompletadas> pistasCompletadas;
+    private Date inicioHistoria;
+
+
+
+    public String getIdHistoria(){
+        return this.idHistoria;
+    }
+
+    public ArrayList<PistasCompletadas> getPistasCompletadas {
+        return this.getPistasCompletadas;
+    }
+    public Date getInicioHistoria(){
+        return this.inicioHistoria;
+    }
+
+}
